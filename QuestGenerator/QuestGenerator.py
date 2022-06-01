000943ed66f87ecbb4e1d39e0f79ec3a19123bc7
Julia May <julia.may@pearson.com>
2022-06-01 13:30:23 +0200

    add mutation mechanism

--- QuestGenerator/QuestGenerator.py
+++ QuestGenerator/QuestGenerator.py
@@ -3,8 +3,8 @@ from PlotGenerator.DomainDatabase.DomainDatabase import DomainDatabase
 from PlotGenerator.QuestGenerator.GeneticAlgorithm import GeneticAlgorithm
 
 dd = DomainDatabase("PlotGenerator/Domain/World.xml")
-ga = GeneticAlgorithm(dd)
-quests_with_plans = ga(generations=5, num_quests=5, desired_story_arc=None)
+ga = GeneticAlgorithm(dd, population_size=10, mutation_prob=0.2, elitism_factor=0.2, start_size=(1, 5), goal_size=(1, 5))
+quests_with_plans = ga(generations=1, num_quests=5, desired_story_arc=None)
 
 for quest_with_plan in quests_with_plans:
     print(f"Start: {quest_with_plan[0][0]}")
